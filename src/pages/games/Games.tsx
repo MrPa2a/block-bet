8fc5aef5d549f571c7fe3b7c0bdac2e38a27ab63
Pierre-Antoine Boulat <pa2a@orange.fr>
2023-05-31 14:40:19 +0200

    create game selector

--- src/pages/games/Games.tsx
+++ src/pages/games/Games.tsx
@@ -1,16 +1,61 @@
 import "./Games.scss";
 
 import SimpleDice from "../../components/games/dice-game/simple-dice/SimpleDice";
+import { useState } from 'react';
 
-function Games () {
-    return ( 
+import { ReactComponent as DiceLogo } from "/public/dice.svg";
+import { ReactComponent as SliderLogo } from "/public/slider.svg";
+
+interface GameElement {
+    name: string;
+    logo: JSX.Element;
+    component: JSX.Element;
+}
+
+function Games() {
+
+    const GAMES: GameElement[] = [
+        {
+            name: "Dice",
+            logo: <DiceLogo/>,
+            component: <SimpleDice />,
+        },
+        {
+            name: "Slider",
+            logo: <SliderLogo/>,
+            component: <SimpleDice />,
+        },
+    ];
+
+    const [selectedGame, setSelectedGame] = useState(GAMES[0]);
+
+    return (
         <div className="games">
             <h2>Games</h2>
+            <div className="game-carrousel">
+                {
+                    GAMES.map((game: GameElement) => {
+                        return <>
+                            <div 
+                                className={`game ${selectedGame.name === game.name ? 'selected' : ''}`}
+                                onClick={() => setSelectedGame(game)}
+                            >
+                                <div className="logo">
+                                    {game.logo}
+                                </div>
+                                <div className="name">
+                                    {game.name}
+                                </div>
+                            </div>
+                        </>
+                    })
+                }
+            </div>
             <div className="games-container">
-                <SimpleDice />
+                {selectedGame.component}
             </div>
         </div>
-     );
+    );
 }
 
 export default Games;
